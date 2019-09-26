1e125c2f43b328630550e7c6b71e9df4e4c3cdac
fikrimaualana <55828126+Fikrimaulana15@users.noreply.github.com>
2019-09-26 17:47:57 +0700

    selesai

---
+++ src/pbo2/pkg10118074/latihanrelational/PBO210118074LatihanRelational.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118074.latihanrelational;
+
+/**
+ *
+ * @author ASUS
+ * NAMA : FikriMaulana
+ * KELAS : IF-2
+ * NIM : 10118074
+ */
+public class PBO210118074LatihanRelational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int bil1 = 10;
+       int bil2 = 20;
+        System.out.println("s == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " +(bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+        
+    }
+    
+}
